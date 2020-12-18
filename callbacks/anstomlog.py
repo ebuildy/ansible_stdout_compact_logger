995d120663bc8a71bb743e4c03a63bacfc86c8b9
Thomas Decaux <ebuildy@gmail.com>
2020-12-18 17:02:08 +0100

    Tree view spaces
    Create kind of tree views, to represent includeTasks, block etc...

--- callbacks/anstomlog.py
+++ callbacks/anstomlog.py
@@ -233,7 +233,15 @@ class CallbackModule(CallbackBase):
                 sectionName = os.path.splitext(os.path.basename(task.get_path()))[0]
             else:
                 sectionName = task._role.get_name()
-            self._open_section("  ↳ {} : {}".format(sectionName, task.name))
+            
+            tabs = ' '
+            pt = task
+
+            while pt is not None:
+                tabs = tabs + ' '
+                pt = pt._parent
+
+            self._open_section(tabs + "  ↳ {} : {}".format(sectionName, task.name))
         else:
             self._open_section(task.get_name(), task.get_path())
 
